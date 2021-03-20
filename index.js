62369d2c98346abb7ea906093cea603d4f2d8a35
jonathan <jhector39@gmail.com>
2021-03-19 23:38:48 -0400

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names=['Lisa','Kaitlin','Jan'];
+const occasion=['suprise'];
+
+function writeCards(names,occasion){
+    const notes=[]
+    for (let i=0;i<names.length;i++){
+        const note =`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+        notes.push(note);
+        debugger;
+    }
+    return notes;
+}
+
+function countDown(number) {
+   for(let i=number; i>=0; i--){
+       console.log(i);
+   }
+}
